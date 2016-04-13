3aff9b7cda31532d720ba632245d97f8675cc29c
Ryan McGuire <ryan@enigmacurry.com>
2016-04-13 19:31:37 -0400

    user_bootstrap should run dotfiles state first before highstate to accomodate any user defined states and pillar data.

--- user_bootstrap.sh
+++ user_bootstrap.sh
@@ -1,3 +1,6 @@
 BASEDIR=$(dirname "$0")
-salt-call -c $BASEDIR/salt/config --local state.highstate 2>&1 | tee ~/install.log
+# Run dotfiles state first, as the user may have states and pillar data defined there:
+salt-call -c $BASEDIR/salt/config --local state.apply dotfiles 2>&1 | tee ~/install.log
+# Now run the entire highstate:
+salt-call -c $BASEDIR/salt/config --local state.highstate 2>&1 | tee -a ~/install.log
 
